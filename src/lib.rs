a87d2c627b6efea99974cd891469d208cf4369f9
Vlad2001_MFS <kopytin.vladislav2001@yandex.ru>
2020-11-15 16:58:21 +0300

    move profile_block macro to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,29 @@ extern crate flume;
 mod profiler;
 
 pub use profiler::*;
+
+#[macro_export]
+macro_rules! profile_block {
+    () => {
+        let _profiler_block_guard = $crate::ProfilerBlockGuard::new({
+            fn f() {}
+
+            #[inline]
+            fn type_name_of_val<T>(_: T) -> &'static str {
+                std::any::type_name::<T>()
+            }
+
+            let name = type_name_of_val(f);
+            &name[..name.len() - 3]
+        });
+    };
+    (name $block_name:literal) => {
+        let _profiler_block_guard = $crate::ProfilerBlockGuard::new($block_name);
+    };
+    (if_feature $name:literal) => {
+        #[cfg(feature = $name)] profile_block!();
+    };
+    (if_feature $feature_name:literal, name $block_name:literal) => {
+        #[cfg(feature = $feature_name)] profile_block!($block_name);
+    };
+}
--- src/profiler.rs
+++ src/profiler.rs
@@ -13,44 +13,6 @@ use std::{
     sync::Mutex,
 };
 
-#[macro_export]
-macro_rules! profile_block {
-    () => {
-        let _profiler_block_guard = $crate::ProfilerBlockGuard::new({
-            fn f() {}
-
-            #[inline]
-            fn type_name_of_val<T>(_: T) -> &'static str {
-                std::any::type_name::<T>()
-            }
-            
-            let name = type_name_of_val(f);
-            &name[..name.len() - 3]
-        });
-    };
-    (name $block_name:literal) => {
-        let _profiler_block_guard = $crate::ProfilerBlockGuard::new($block_name);
-    };
-    (if_feature $name:literal) => {
-        #[cfg(feature = $name)]
-        let _profiler_block_guard = $crate::ProfilerBlockGuard::new({
-            fn f() {}
-
-            #[inline]
-            fn type_name_of_val<T>(_: T) -> &'static str {
-                std::any::type_name::<T>()
-            }
-            
-            let name = type_name_of_val(f);
-            &name[..name.len() - 3]
-        });
-    };
-    (if_feature $feature_name:literal, name $block_name:literal) => {
-        #[cfg(feature = $feature_name)]
-        let _profiler_block_guard = $crate::ProfilerBlockGuard::new($block_name);
-    };
-}
-
 struct BlockStatReport {
     name: String,
     avg_time: Duration,
